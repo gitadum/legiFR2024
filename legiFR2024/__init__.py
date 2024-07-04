8fe2e75ca002a2359b39e31ba977bd99f2b05fb4
Adrien Dumont <63854962+gitadum@users.noreply.github.com>
2024-07-04 11:39:07 +0200

    Passage à la version 2.1.0

--- legiFR2024/__init__.py
+++ legiFR2024/__init__.py
@@ -4,5 +4,5 @@ from .prep import Scrutin
 from .prep import PremierTour
 from .prep import SecondTour
 
-__version__ = "2.1.0b"
+__version__ = "2.1.0"
 __all__ = ["Scrutin", "PremierTour", "SecondTour"]
